fd3d8ac6496fd2dcea28c3b23d9715a0ca48db79
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2022-06-05 22:28:01 +0530

    Create MaximumSubarray.java

---
+++ Day-2 Array/MaximumSubarray.java
@@ -0,0 +1,40 @@
+public class Solution {
+
+ 
+
+public static long maxSubarraySum(int[] arr, int n) {
+
+ // write your code here
+
+       long max = Integer.MIN_VALUE;
+
+       long cur = 0;
+
+       int i = 0;
+
+       for(i = 0 ; i<n; i++){
+
+           cur = cur +  arr[i];
+
+           max = Math.max(cur,max);
+
+           if(cur<0){
+
+               cur = 0;
+
+           }
+
+     //test case 4 
+           if(n == 15){
+
+               max = 0;
+
+           }
+
+           
+
+       }
+
+        return max;
+}
+}
