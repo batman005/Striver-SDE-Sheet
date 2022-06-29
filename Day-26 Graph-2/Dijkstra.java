049a0870bf2a6a0ce0f43e21d1c654d8e3c4ea15
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2022-06-29 15:26:49 +0530

    Graph-2

---
+++ Day-26 Graph-2/Dijkstra.java
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
+            for(int i=0;i<V;i++)
+            {
+                adj.add(new ArrayList<ArrayList<Integer>>());
+            }
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<Integer>();
+                ArrayList<Integer> t2 = new ArrayList<Integer>();
+                t1.add(v);
+                t1.add(w);
+                t2.add(u);
+                t2.add(w);
+                adj.get(u).add(t1);
+                adj.get(v).add(t2);
+            }
+            
+            int S = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            
+            int[] ptr = ob.dijkstra(V, adj, S);
+            
+            for(i=0; i<V; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+        int dist[] = new int[V];
+        boolean fin[] = new boolean[V];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{S,0});
+        dist[S] = 0;
+        while( !pq.isEmpty()){
+            int[] cur = pq.poll();
+            fin[cur[0]] = true;
+            int wei = cur[1];
+         for( ArrayList<Integer> x : adj.get(cur[0])){
+                if(!fin[ x.get(0) ] && dist[ x.get(0) ] >  wei + x.get(1)){
+                   dist[ x.get(0) ] =  wei + x.get(1) ;
+                   pq.offer( new int[]{ x.get(0), dist[ x.get(0) ]});
+                }
+              }
+           }
+        return dist;
+    }
+}
+
